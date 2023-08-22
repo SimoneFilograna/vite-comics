1eeb266b7bd5b02c5a345a4d388aabc5c02391e2
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-08-22 14:46:30 +0200

    create-basic-template-app-vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <script>
+import TheHeader from "../src/components/TheHeader.vue";
+import TheFooter from "../src/components/TheFooter.vue";
+
+export default{
+  components: {
+    TheHeader,
+    TheFooter
+
+  }
+}
 </script>
 
 <template>
+
+  <TheHeader></TheHeader>
+
+  <main></main>
+
+  <TheFooter></TheFooter>
   
 </template>
 
