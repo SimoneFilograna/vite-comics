eaeab4ddc164733a56da80fa92df1b5939556065
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-08-22 15:59:40 +0200

    main-finished

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,31 @@ export default{
 
   <TheHeader></TheHeader>
 
-  <main></main>
+  <main>
+    <div class="container">
+      <div class="box">
+        <h2>Content goes here</h2>
+      </div>
+    </div>
+  </main>
 
   <TheFooter></TheFooter>
-  
+
 </template>
 
-<style>
+<style lang="scss">
+  @use "../src/styles/partials/variables";
+  @use "../src/styles/partials/mixins";
+
+  main{
+    background-color: variables.$color-secondary-dark;
+
+    .box{
+      padding: 4rem 0;
+      h2{
+        color: white;
+      }
+    }
+  }
 
 </style>
