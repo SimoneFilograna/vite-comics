6376b5c8460bd1acd5bbb22000a0e43cd3700887
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-08-23 15:44:35 +0200

    insert-card

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <script>
 import TheHeader from "../src/components/TheHeader.vue";
 import TheFooter from "../src/components/TheFooter.vue";
-import BlueBar from "../src/components/BlueBar.vue"
+import BlueBar from "../src/components/BlueBar.vue";
+import Comics from "../src/db/dc-comics.json";
+import ComicCard from "../src/components/ComicCard.vue";
 
 export default{
   components: {
     TheHeader,
     TheFooter,
     BlueBar,
+    ComicCard,
+  },
 
+  data(){
+    return{
+      Comics,
+    }
   }
+
 }
 </script>
 
@@ -18,15 +27,25 @@ export default{
   <body>
     <TheHeader></TheHeader>
 
-    <div class="jumbotron"></div>
-
+    
     <main>
+      <div class="jumbotron"></div>
+
       <div class="container">
-        <div class="box">
-          <h2>Content goes here</h2>
+        <div class="row">
+          <div class="col-2" v-for="(singleComic, i) in Comics" :key="i">
+
+            <ComicCard
+             :img="singleComic.thumb"
+             :name="singleComic.series" >
+            </ComicCard>
+
+          </div>
         </div>
       </div>
+
       <BlueBar></BlueBar>
+
     </main>
 
     <TheFooter></TheFooter>
---
+++ src/components/ComicCard.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    props: {
+        img: String,
+        name: String,
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <img :src="img" class="card-img-top" alt="">
+        <div class="card-body">
+            <p class="card-text">{{name}}</p>
+        </div>
+    </div>
+</template>
