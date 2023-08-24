16c55fd01b3f43e038fcc2bd63c5ef411e7b247f
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-08-24 09:07:38 +0200

    need-fix

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -63,6 +63,7 @@ export default{
                 <ul class="nav">
                     <li class="nav-item" v-for="singleItem in navItem">
                         <a href="#" class="nav-link p-3">{{singleItem.name}}</a>
+                        <div class="line-under"></div>
                     </li>
                 </ul>
             </div>
@@ -105,7 +106,22 @@ export default{
     .nav-item{
         font-size: .8rem;
         font-weight: 700;
+        position: relative;
+
+        &:hover .line-under {
+            opacity: 1
+        }
+
     };
+
+    .line-under{
+        width: 100%;
+        border: 3px solid variables.$color-primary;
+        position: absolute;
+        bottom: -2rem;
+        opacity: 0;
+
+    }
             
         
 
